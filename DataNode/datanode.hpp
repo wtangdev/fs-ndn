de21c7d9a12d9ef1c4fe7007c955cfac2659f57f
WeiTang <iw.tang@outlook.com>
2019-03-16 10:04:00 +0800

    rename

--- DataNode/datanode.hpp
+++ DataNode/datanode.hpp
@@ -7,27 +7,12 @@
 
 #include "../INode/inodefile.hpp"
 
-
 class NameNode{
 public:
-    NameNode();
-    // 创建空文件
-    int addEmptyFile(string name, time_t mtime, time_t atime, time_t ctime);
-    // 创建含内容的文件
-    int addNewFile(string name, unsigned char * content, int size, time_t mtime, time_t atime, time_t ctime);
-    int delFile(string name);
-    int delDir(string prefix);
-
-    int getFileSize(string name);
-
-    int writeToFile(string name, unsigned char * content, int size);
-    int readFromFile(string name, unsigned char * buffer, int size);
-    vector<string> showChildren(string prefix);
 
 protected:
 
 private:
-    vector<INodeFile> names;    // 当前节点管理的所有文件
 };
 
 
